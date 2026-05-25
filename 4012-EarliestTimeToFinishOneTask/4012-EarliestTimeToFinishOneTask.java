b51cd84203880c3af322acf0c0cac3e741d8eb9b
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:13:42 +0530

    Create 4012-EarliestTimeToFinishOneTask

---
+++ 4012-EarliestTimeToFinishOneTask/4012-EarliestTimeToFinishOneTask.java
@@ -0,0 +1,11 @@
+// Last updated: 5/25/2026, 11:13:20 AM
+class Solution {
+    public int earliestTime(int[][] tasks) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<tasks.length;i++){
+            int sum=tasks[i][0]+tasks[i][1];
+            min=Math.min(min,sum);
+        }
+        return min;
+    }
+}

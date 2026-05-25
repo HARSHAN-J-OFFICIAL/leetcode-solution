72c27d31edc1052465c1024517396e13c2deadfe
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:21:41 +0530

    Create 54-SpiralMatrix

---
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -0,0 +1,37 @@
+// Last updated: 5/25/2026, 11:21:19 AM
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> l=new ArrayList<>();
+
+        int left=0;int right=matrix[0].length-1;
+        int top=0;int bottom=matrix.length-1;
+
+        while(top<=bottom && left<=right){
+            // left to right
+            for(int i=left;i<=right;i++){
+                l.add(matrix[top][i]);
+            }
+            top++;
+            // top to bottom
+            for(int i=top;i<=bottom;i++){
+                l.add(matrix[i][right]);
+            }
+            right--;
+            // right to left
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    l.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            // bottom to top
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    l.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return l;
+    }
+}

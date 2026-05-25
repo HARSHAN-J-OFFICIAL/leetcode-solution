36b04e7e825461140e11f22765bf343a99f2b77a
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:17:53 +0530

    Create 513-FindBottomLeftTreeValue

---
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.java
@@ -0,0 +1,35 @@
+// Last updated: 5/25/2026, 11:17:31 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDepth = -1;
+    int answer = 0;
+    public int findBottomLeftValue(TreeNode root) {
+        dfs(root, 0);
+        return answer;
+    }
+    public void dfs(TreeNode root, int depth) {
+        if(root == null) {
+            return;
+        }
+        if(depth > maxDepth) {
+            maxDepth = depth;
+            answer = root.val;
+        }
+        dfs(root.left, depth+1);
+        dfs(root.right, depth+1);
+    }
+}

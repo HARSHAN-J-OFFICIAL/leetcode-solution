a5e3fbbca29dac2b6962260667c86620a7edb4e7
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:22:29 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,21 @@
+# Last updated: 5/25/2026, 11:22:07 AM
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+        
+        for char in s:
+            if char in pairs.values():  
+                stack.append(char)
+            elif char in pairs:  
+                if not stack or stack[-1] != pairs[char]:
+                    return False
+                stack.pop()
+            else:
+                return False  
+        
+        return not stack  
+
+
+
+        # nonono

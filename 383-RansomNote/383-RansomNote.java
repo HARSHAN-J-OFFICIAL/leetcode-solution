a8b68cc9c2614579b0c4b69a0a385e021e15c828
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:18:25 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,20 @@
+// Last updated: 5/25/2026, 11:18:03 AM
+class Solution {
+    public boolean canConstruct(String s, String t) {
+        int fre[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            fre[c-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            char c=t.charAt(i);
+            fre[c-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(fre[i]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

1535d826fb8142a5339c45c5f2442db33356b159
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:16:59 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,18 @@
+// Last updated: 5/25/2026, 11:16:37 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(target==nums[mid])
+                return mid;
+            else if(target<nums[mid]){
+                high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        return -1;
+    }
+}

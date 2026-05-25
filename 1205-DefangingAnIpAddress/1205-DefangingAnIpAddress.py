65e011fcd07fc1dbeff996e0772c0508facb0902
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:16:03 +0530

    Create 1205-DefangingAnIpAddress

---
+++ 1205-DefangingAnIpAddress/1205-DefangingAnIpAddress.py
@@ -0,0 +1,11 @@
+# Last updated: 5/25/2026, 11:15:41 AM
+class Solution(object):
+    def defangIPaddr(self, address):
+        return address.replace('.','[.]')
+
+
+    # n=input('address = ')
+    # res=defangIPaddr(n)
+    # print(res)
+        
+        

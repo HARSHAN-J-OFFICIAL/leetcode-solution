445a9fbc2bcf5ca6c604dc306b3774dcf83d235b
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:13:48 +0530

    Create 3555-FinalArrayStateAfterKMultiplicationOperationsI

---
+++ 3555-FinalArrayStateAfterKMultiplicationOperationsI/3555-FinalArrayStateAfterKMultiplicationOperationsI.java
@@ -0,0 +1,15 @@
+// Last updated: 5/25/2026, 11:13:26 AM
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for (int i = 0; i < k; i++) {
+            int ind = 0;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[j] < nums[ind]) {
+                    ind = j;
+                }
+            }
+            nums[ind] *= multiplier;
+        }
+        return nums;
+    }
+}

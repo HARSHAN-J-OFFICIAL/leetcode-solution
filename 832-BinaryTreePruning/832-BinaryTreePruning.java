aa206d99ca16f7a7b56a553028281647582ae45b
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:16:52 +0530

    Create 832-BinaryTreePruning

---
+++ 832-BinaryTreePruning/832-BinaryTreePruning.java
@@ -0,0 +1,34 @@
+// Last updated: 5/25/2026, 11:16:30 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode delete(TreeNode root){
+        if(root==null){
+            return null;
+        }
+        root.left=delete(root.left);
+        root.right=delete(root.right);
+        if(root.left==null && root.right==null && root.val==0){
+            return null;
+        }
+        return root;
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        return delete(root);
+    }
+}

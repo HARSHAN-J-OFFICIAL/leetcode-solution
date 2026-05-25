ea366b695ca37ab0ff77b0344227791c69011adf
HARSHAN-J <harshanjaganathan0406@gmail.com>
2026-05-25 11:21:29 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.py
@@ -0,0 +1,21 @@
+# Last updated: 5/25/2026, 11:21:08 AM
+class Solution(object):
+    def mySqrt(self, x):
+        if x<2:
+            return x
+        left,right=1,x//2
+        while left<=right:
+            mid=(left+right)//2
+            square=mid*mid
+            if square==x:
+                return mid
+            elif square<x:
+                left=mid+1
+            else:
+                right=mid-1
+        return right
+        """
+        :type x: int
+        :rtype: int
+        """
+        
